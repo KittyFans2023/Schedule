062bec6f1d674492e8823ec9a3521a46cbe9698b
KittyFans2023 <149091080+KittyFans2023@users.noreply.github.com>
2023-10-30 16:38:13 +0300

    Update db.go

--- db/db.go
+++ db/db.go
@@ -2,12 +2,13 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func Make_db(json_data map[string]map[string]map[int]map[string][]string) {
+func Make_db(data []map[string]interface{}) {
 	// Use the SetServerAPIOptions() method to set the Stable API version to 1
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI("mongodb+srv://projectlaim2023:jTpSqRamIKn3UTT2@cluster0.lxtqivz.mongodb.net/").SetServerAPIOptions(serverAPI)
@@ -25,6 +26,29 @@ func Make_db(json_data map[string]map[string]map[int]map[string][]string) {
 	}()
 	client.Database("CFU").Collection("schedule").Drop(context.TODO()) //ощищаем нашу коллекцию с расписанием
 	collection := client.Database("CFU").Collection("schedule")
-	collection.InsertOne(context.TODO(), json_data) //записываем в нее новое
+	for _, e := range data {
+		collection.InsertOne(context.Background(), e) //записываем в нее новое
+	}
+}
+
+func Info_about(group ...string) {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://projectlaim2023:jTpSqRamIKn3UTT2@cluster0.lxtqivz.mongodb.net/").SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	collection := client.Database("CFU").Collection("schedule")
+	if len(group) != 0 {
+		fmt.Println(collection.FindOne(context.Background(), group[0]))
+	}
 
 }
